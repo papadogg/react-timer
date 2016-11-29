fa3ae0bc9295bc66e1d3a92ba6039ee9afd7a421
Victor <papadoginc@gmail.com>
2016-11-29 18:22:41 +0300

    Add Clock

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Clock from 'Clock';
+
+class Countdown extends React.Component {
+    render() {
+        return (
+            <div>
+                <Clock totalSeconds={129}/>
+            </div>
+        );
+    }
+}
+
+module.exports = Countdown;
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -8,8 +8,8 @@ const Nav = () =>{
             <div className="top-bar-left">
                 <ul className="menu">
                     <li className="menu-text">React Timer App</li>
-                    <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Timer</IndexLink></li>
-                    <li><Link  to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Countdown</Link></li>                        
+                    <li><IndexLink to="/" activeClassName="active-link" >Timer</IndexLink></li>
+                    <li><Link  to="/countdown" activeClassName="active-link" >Countdown</Link></li>                        
                 </ul>
             </div>
             <div className="top-bar-right">
